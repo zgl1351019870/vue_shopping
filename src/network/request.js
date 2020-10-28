8b079f129d0eb993bf2ea144b6f0e0fa9e8ef7ba
zgl1351019870 <56183110+zgl1351019870@users.noreply.github.com>
2020-10-28 21:47:16 +0800

    Update request.js
    接口暂不提供

--- src/network/request.js
+++ src/network/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function request(config){
   const instance = axios.create({
-    baseURL:'http://152.136.185.210:8000/api/w6',
+    baseURL:'xxx',
     timeout:10000
   });
   return instance(config);
